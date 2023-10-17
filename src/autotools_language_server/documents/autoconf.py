c52249683582be4d06ef621af26df36f5605ca66
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-10-17 20:14:46 +0800

    :truck: Move api to documents

--- src/autotools_language_server/documents/autoconf.py
+++ src/autotools_language_server/documents/autoconf.py
@@ -1,4 +1,4 @@
-r"""Api Make
+r"""Autoconf
 ============
 """
 import os
@@ -7,8 +7,12 @@ from gzip import decompress
 
 from platformdirs import site_data_dir
 
+START = " -- Macro: "
+BEGIN = "‘"
+END = "’"
 
-def get_macro(line: str, begin: str = "$(", end: str = ")") -> str:
+
+def get_macro(line: str, begin: str = START, end: str = "") -> str:
     r"""Get macro.
 
     :param line:
@@ -19,10 +23,10 @@ def get_macro(line: str, begin: str = "$(", end: str = ")") -> str:
     :type end: str
     :rtype: str
     """
-    return line.strip("'").lstrip(begin).rstrip(end).split()[0]
+    return line.lstrip(begin).rstrip(end).split("(")[0].strip()
 
 
-def match(line: str, begin: str = "'", end: str = "'") -> bool:
+def match(line: str, begin: str = START, end: str = "") -> bool:
     r"""Match.
 
     :param line:
@@ -33,12 +37,10 @@ def match(line: str, begin: str = "'", end: str = "'") -> bool:
     :type end: str
     :rtype: bool
     """
-    macro = line.strip("'")
     return (
         line.startswith(begin)
         and line.endswith(end)
-        and (not macro.startswith("-") or macro == "-include")
-        and (macro not in ["0", "1", "2"])
+        and line.lstrip(begin).split("_")[0] in ["AC", "AM", "AH", "m4"]
     )
 
 
@@ -56,7 +58,7 @@ def reset(
     :rtype: tuple[dict[str, str], list[str], list[str]]
     """
     for macro in _macros:
-        macros[macro] = "\n".join(line for line in lines if line)
+        macros[macro] = "\n".join(lines)
     _macros = []
     lines = []
     return macros, _macros, lines
@@ -77,26 +79,54 @@ def get_content(filename) -> str:
     return content
 
 
-def init_make_document() -> dict[str, str]:
-    r"""Init make document.
+def init_autoconf_document() -> dict[str, str]:
+    r"""Init autoconf document.
 
     :rtype: dict[str, str]
     """
     macros = {}
 
-    _lines = get_content("make.info-2").splitlines()
+    _lines = get_content("autoconf.info").splitlines()
     _macros = []
     lines = []
     lastline = ""
     for line in _lines:
+        # -- Macro: AC_INIT (PACKAGE, VERSION, [BUG-REPORT], [TARNAME], [URL])
+        #     ...
         if match(line) and not match(lastline):
             macros, _macros, lines = reset(macros, _macros, lines)
             _macros += [get_macro(line)]
             lines += [line]
+        # -- Macro: AC_CONFIG_MACRO_DIRS (DIR1 [DIR2 ... DIRN])
+        # -- Macro: AC_CONFIG_MACRO_DIR (DIR)
+        #     ...
         elif match(line) and match(lastline):
             _macros += [get_macro(line)]
             lines += [line]
-        elif _macros and (line.startswith("     ") or line == ""):
+        # ...
+        #
+        # ...
+        # or not
+        #    text indented 3 spaces is not document
+        elif _macros and (len(line) < 4 or line[4] == " "):
+            lines += [line]
+        else:
+            macros, _macros, lines = reset(macros, _macros, lines)
+        lastline = line
+
+    _lines = get_content("automake.info-1").splitlines()
+    _macros = []
+    lines = []
+    lastline = ""
+    for line in _lines:
+        if match(line, BEGIN, END) and not match(lastline, BEGIN, END):
+            macros, _macros, lines = reset(macros, _macros, lines)
+            _macros += [get_macro(line, BEGIN, END)]
+            lines += [line]
+        elif match(line, BEGIN, END) and match(lastline, BEGIN, END):
+            _macros += [get_macro(line, BEGIN, END)]
+            lines += [line]
+        elif _macros and (len(line) < 1 or line[0] == " "):
             lines += [line]
         else:
             macros, _macros, lines = reset(macros, _macros, lines)
--- src/autotools_language_server/server.py
+++ src/autotools_language_server/server.py
@@ -1,10 +1,9 @@
 r"""Server
 ==========
 """
-import json
 import os
 import re
-from typing import Any, Literal, Tuple
+from typing import Any, Tuple
 from urllib.parse import unquote, urlparse
 
 from lsprotocol.types import (
@@ -28,72 +27,11 @@ from lsprotocol.types import (
     Range,
     TextDocumentPositionParams,
 )
-from platformdirs import user_cache_dir
 from pygls.server import LanguageServer
 
 from ._tree_sitter import get_parser
 from .diagnostics import diagnostic
-
-
-def check_extension(
-    path: str,
-) -> Literal["config", "make", ""]:
-    r"""Check extension.
-
-    :param path:
-    :type path: str
-    :rtype: Literal["config", "make", ""]
-    """
-    if (
-        path.split(os.path.extsep)[-1] in ["mk"]
-        or os.path.basename(path).split(os.path.extsep)[0] == "Makefile"
-    ):
-        return "make"
-    if os.path.basename(path) == "configure.ac":
-        return "config"
-    return ""
-
-
-def get_document(
-    method: Literal["builtin", "cache", "system"] = "builtin"
-) -> dict[str, tuple[str, str]]:
-    r"""Get document. ``builtin`` will use builtin autotools.json. ``cache``
-    will generate a cache from
-    ``${XDG_CACHE_DIRS:-/usr/share}/info/autoconf.info.gz``,
-    ``${XDG_CACHE_DIRS:-/usr/share}/info/automake.info-1.gz``,
-    ``${XDG_CACHE_DIRS:-/usr/share}/info/make.info-2.gz``.
-    ``system`` is same as ``cache`` except it doesn't generate cache. Some
-    distribution's autotools doesn't contain textinfo. So we use ``builtin`` as
-    default.
-
-    :param method:
-    :type method: Literal["builtin", "cache", "system"]
-    :rtype: dict[str, tuple[str, str]]
-    """
-    if method == "builtin":
-        file = os.path.join(
-            os.path.join(
-                os.path.join(os.path.dirname(__file__), "assets"), "json"
-            ),
-            "autotools.json",
-        )
-        with open(file, "r") as f:
-            document = json.load(f)
-    elif method == "cache":
-        from .api import init_document
-
-        if not os.path.exists(user_cache_dir("autotools.json")):
-            document = init_document()
-            with open(user_cache_dir("autotools.json"), "w") as f:
-                json.dump(document, f)
-        else:
-            with open(user_cache_dir("autotools.json"), "r") as f:
-                document = json.load(f)
-    else:
-        from .api import init_document
-
-        document = init_document()
-    return document
+from .documents import check_extension, get_document
 
 
 class AutotoolsLanguageServer(LanguageServer):
@@ -249,10 +187,8 @@ class AutotoolsLanguageServer(LanguageServer):
                 word = (
                     line[m.start() : end],
                     Range(
-                        start=Position(
-                            line=position.line, character=m.start()
-                        ),
-                        end=Position(line=position.line, character=end),
+                        Position(position.line, m.start()),
+                        Position(position.line, end),
                     ),
                 )
                 return word
