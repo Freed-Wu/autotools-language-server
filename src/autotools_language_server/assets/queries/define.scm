2bc19427cede55be2e150ebe74354bbb51a14a43
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-10-30 21:19:27 +0800

    :zap: Add tree-sitter-query

---
+++ src/autotools_language_server/assets/queries/define.scm
@@ -0,0 +1,27 @@
+(define_directive
+  name: (word) @function.def
+  ) @function
+
+(variable_assignment
+  name: (word) @variable.def
+  ) @variable
+
+(rule
+  (targets
+    (word) @rule.def
+    )
+  normal: (prerequisites
+            (word) @rule.call
+            )
+  ) @rule
+
+(function_call
+  (arguments
+    argument: (text) @function.arg
+    )
+  )
+
+(variable_reference
+  (word) @variable.call
+  )
+; ex: filetype=query
