f21ac2562f1b95e8dbc26c583f291cf99f0bee61
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-03-05 16:42:25 +0800

    :children_crossing: Improve shell completions

--- src/autoconf_language_server/__main__.py
+++ src/autoconf_language_server/__main__.py
@@ -3,13 +3,17 @@ r"""This module can be called by
 """
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from contextlib import suppress
 from datetime import datetime
 
 from make_language_server import __version__
 
 from . import FILETYPE
 from . import __name__ as NAME
+
+try:
+    import shtab
+except ImportError:
+    import _shtab as shtab
 
 NAME = NAME.replace("_", "-")
 VERSION = rf"""{NAME} {__version__}
@@ -21,41 +25,43 @@ Report bugs to <wuzhenyu@ustc.edu>.
 """
 
 
-def get_parser():
+def get_parser() -> ArgumentParser:
     r"""Get a parser for unit test."""
     parser = ArgumentParser(
         epilog=EPILOG,
         formatter_class=RawDescriptionHelpFormatter,
     )
-    with suppress(ImportError):
-        import shtab
-
-        shtab.add_argument_to(parser)
+    shtab.add_argument_to(parser)
     parser.add_argument("--version", version=VERSION, action="version")
     parser.add_argument(
         "--generate-schema",
         choices=FILETYPE.__args__,  # type: ignore
-        help="generate schema json",
+        help="generate schema in an output format",
+    )
+    parser.add_argument(
+        "--output-format",
+        choices=["json", "yaml", "toml"],
+        default="json",
+        help="output format: %(default)s",
     )
     parser.add_argument(
         "--indent",
         type=int,
         default=2,
-        help="generated json's indent",
+        help="generated json, yaml's indent, ignored by toml: %(default)s",
     )
     parser.add_argument(
         "--color",
         choices=["auto", "always", "never"],
         default="auto",
-        help="when to display color",
+        help="when to display color, default: %(default)s",
     )
     return parser
 
 
-def main():
+def main() -> None:
     r"""Parse arguments and provide shell completions."""
-    parser = get_parser()
-    args = parser.parse_args()
+    args = get_parser().parse_args()
 
     if args.generate_schema:
         from lsp_tree_sitter.utils import pprint
